a7d2b31752cfed104e6ff64ec663d437b3361c5e
James Kenney <jkenney2@charter.net>
2024-09-01 23:38:42 -0400

    Added Spline

--- TeamCode/src/org/firstinspires/ftc/teamcode/mybot/MyAuto.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/mybot/MyAuto.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.mybot;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.util.MotionProfile;
 import org.firstinspires.ftc.teamcode.util.Pose;
+import org.firstinspires.ftc.teamcode.util.QuinticSpline2D;
 
 
 public abstract class MyAuto extends LinearOpMode {
@@ -81,6 +83,263 @@ public abstract class MyAuto extends LinearOpMode {
 
         bot.setDrivePower(0, 0, 0);
     }
+
+
+    /**
+     * Drive quintic spline from current position and heading to target position and heading.
+     * @param mProf     Min and max speed, acceleration
+     * @param endX      target X coordinate
+     * @param endY      target Y coordinate
+     * @param endHeadingDegrees     Target final TRAVEL DIRECTION
+     * @param tolerance             target final coordinate tolerance, inches
+     * @param reverse               If true, drive in reverse
+     */
+    public void splineTo(MotionProfile mProf, double endX, double endY,
+                         double endHeadingDegrees, double tolerance, boolean reverse){
+        bot.updateOdometry();
+        double startHeadingRadians = bot.getPose().h;
+        if (reverse){
+            startHeadingRadians = AngleUnit.normalizeRadians(startHeadingRadians + Math.PI);
+        }
+        double endHeadingRadians = Math.toRadians(endHeadingDegrees);
+        VectorF endVec = new VectorF((float)endX, (float)endY);
+        QuinticSpline2D spline = new QuinticSpline2D(bot.getPose().x, bot.getPose().y,
+                endX, endY, startHeadingRadians, endHeadingRadians);
+        double totalLength = spline.getLength();
+        VectorF prevTargVec = spline.p(0);
+        double s = 0;
+        double d0 = 0;
+        double absDistToEnd = 0;
+        double headingError = 0;
+        VectorF vel;
+        VectorF velRobot;
+        double va = 0;
+
+        while (opModeIsActive()){
+            bot.updateOdometry();
+            VectorF poseVec = new VectorF((float)bot.getPose().x, (float)bot.getPose().y);
+
+            // Find closest point on spline to current pose
+            s = spline.findClosestS(poseVec.get(0), poseVec.get(1), s);
+
+            // Distance (crow flies) between bot pose and end point
+            absDistToEnd = poseVec.subtracted(endVec).magnitude();
+
+            // If at or beyond end of spline or within tolerance, break
+            if (s >= 0.9999999 || absDistToEnd < tolerance) break;
+
+            // Where bot should be now along spline
+            VectorF currTargVec = spline.p(s);
+
+            // Update distance traveled (d0) along spline so far, and find
+            // remaining distance along spline (d1) to end
+            VectorF deltaTargVec = currTargVec.subtracted(prevTargVec);
+            d0 += deltaTargVec.magnitude();
+            if (d0 > totalLength) d0 = totalLength;
+            double d1 = totalLength - d0;
+
+            // targErr is vector from current bot pose to where bot should be on spline
+            VectorF targErr = currTargVec.subtracted(poseVec);
+
+            // travelDir is unit vector giving current direction of travel along spline
+            VectorF travelDir = spline.d1(s);
+            travelDir = travelDir.multiplied(1.0f/travelDir.magnitude());
+
+            // Compute nominal travel speed, v
+            double v0 = Math.sqrt(mProf.vMin * mProf.vMin + 2 * mProf.accel * d0);
+            double v1 = Math.sqrt(mProf.vMin * mProf.vMin + 2 * mProf.accel * d1);
+            double v = Math.min(v0, v1);
+            v = Math.min(v, mProf.vMax);
+
+            // Compute travel velocity, including nominal velocity plus correction
+            vel = travelDir.multiplied((float)v).added(targErr.multiplied(8.0f));
+
+            // Convert vel from field coordinates to robot coordinates
+            velRobot = fieldToBot(vel, bot.getPose().h);
+
+            // Compute target heading at current location on spline, and heading rate of change
+            double targetHeading = spline.getHeading(s);
+            if (reverse) {
+                targetHeading = AngleUnit.normalizeRadians(targetHeading + Math.PI);
+            }
+            double targetHeadingRate = spline.getHeadingRateOfChange(s, v);
+
+            // Compute current heading error
+            headingError = AngleUnit.normalizeRadians(targetHeading - bot.getPose().h);
+
+            // Compute heading speed, including nominal rate of change plus correction
+            va = targetHeadingRate + 4.0 * headingError;
+
+            // Set robot speed
+            bot.setDriveSpeed(velRobot.get(0), velRobot.get(1), va);
+
+            prevTargVec = currTargVec;
+        }
+
+        /*
+         * We have either reached the end of the spline or reached end point within tolerance.
+         * In case tolerance not reached, allow 1 second period of adjustment
+         */
+
+        ElapsedTime et = new ElapsedTime();
+
+        double targetHeading = endHeadingRadians;
+        if (reverse) {
+            targetHeading = AngleUnit.normalizeRadians(targetHeading + Math.PI);
+        }
+
+        while (opModeIsActive() && et.seconds() < 1){
+            bot.updateOdometry();
+            VectorF vecToEnd = endVec.subtracted(new VectorF((float)bot.getPose().x, (float)bot.getPose().y));
+            headingError = AngleUnit.normalizeRadians(targetHeading - bot.getPose().h);
+            if (vecToEnd.magnitude() < tolerance && Math.abs(headingError) < Math.toRadians(3)) break;
+            vel = vecToEnd.multiplied(8);
+            velRobot = fieldToBot(vel, bot.getPose().h);
+            va = 4.0 * headingError;
+            bot.setDriveSpeed(velRobot.get(0), velRobot.get(1), va);
+        }
+
+        bot.setDriveSpeed(0,0,0);
+    }
+
+
+    /**
+     * Drive quintic spline from current position and heading to target position and heading.
+     * @param mProf     Min and max speed, acceleration
+     * @param endX      target X coordinate
+     * @param endY      target Y coordinate
+     * @param endHeadingDegrees     Target final TRAVEL DIRECTION
+     * @param tolerance             target final coordinate tolerance, inches
+     * @param reverse               If true, drive in reverse
+     * @param alpha     Higher alpha -> less turning at the beginning, more at the middle of path
+     * @param beta      Higher beta -> less turning at the end, more at the middle of path
+     */
+    public void splineTo(MotionProfile mProf, double endX, double endY, double endHeadingDegrees,
+                         double tolerance, boolean reverse, double alpha, double beta){
+        bot.updateOdometry();
+        double startHeadingRadians = bot.getPose().h;
+        if (reverse){
+            startHeadingRadians = AngleUnit.normalizeRadians(startHeadingRadians + Math.PI);
+        }
+        double endHeadingRadians = Math.toRadians(endHeadingDegrees);
+        VectorF endVec = new VectorF((float)endX, (float)endY);
+        QuinticSpline2D spline = new QuinticSpline2D(bot.getPose().x, bot.getPose().y,
+                endX, endY, startHeadingRadians, endHeadingRadians, alpha, beta);
+        double totalLength = spline.getLength();
+        VectorF prevTargVec = spline.p(0);
+        double s = 0;
+        double d0 = 0;
+        double absDistToEnd = 0;
+        double headingError = 0;
+        VectorF vel;
+        VectorF velRobot;
+        double va = 0;
+
+        while (opModeIsActive()){
+            bot.updateOdometry();
+            VectorF poseVec = new VectorF((float)bot.getPose().x, (float)bot.getPose().y);
+
+            // Find closest point on spline to current pose
+            s = spline.findClosestS(poseVec.get(0), poseVec.get(1), s);
+
+            // Distance (crow flies) between bot pose and end point
+            absDistToEnd = poseVec.subtracted(endVec).magnitude();
+
+            // If at or beyond end of spline or within tolerance, break
+            if (s >= 0.9999999 || absDistToEnd < tolerance) break;
+
+            // Where bot should be now along spline
+            VectorF currTargVec = spline.p(s);
+
+            // Update distance traveled (d0) along spline so far, and find
+            // remaining distance along spline (d1) to end
+            VectorF deltaTargVec = currTargVec.subtracted(prevTargVec);
+            d0 += deltaTargVec.magnitude();
+            if (d0 > totalLength) d0 = totalLength;
+            double d1 = totalLength - d0;
+
+            // targErr is vector from current bot pose to where bot should be on spline
+            VectorF targErr = currTargVec.subtracted(poseVec);
+
+            // travelDir is unit vector giving current direction of travel along spline
+            VectorF travelDir = spline.d1(s);
+            travelDir = travelDir.multiplied(1.0f/travelDir.magnitude());
+
+            // Compute nominal travel speed, v
+            double v0 = Math.sqrt(mProf.vMin * mProf.vMin + 2 * mProf.accel * d0);
+            double v1 = Math.sqrt(mProf.vMin * mProf.vMin + 2 * mProf.accel * d1);
+            double v = Math.min(v0, v1);
+            v = Math.min(v, mProf.vMax);
+
+            // Compute travel velocity, including nominal velocity plus correction
+            vel = travelDir.multiplied((float)v).added(targErr.multiplied(8.0f));
+
+            // Convert vel from field coordinates to robot coordinates
+            velRobot = fieldToBot(vel, bot.getPose().h);
+
+            // Compute target heading at current location on spline, and heading rate of change
+            double targetHeading = spline.getHeading(s);
+            if (reverse) {
+                targetHeading = AngleUnit.normalizeRadians(targetHeading + Math.PI);
+            }
+            double targetHeadingRate = spline.getHeadingRateOfChange(s, v);
+
+            // Compute current heading error
+            headingError = AngleUnit.normalizeRadians(targetHeading - bot.getPose().h);
+
+            // Compute heading speed, including nominal rate of change plus correction
+            va = targetHeadingRate + 4.0 * headingError;
+
+            // Set robot speed
+            bot.setDriveSpeed(velRobot.get(0), velRobot.get(1), va);
+
+            prevTargVec = currTargVec;
+        }
+
+        /*
+         * We have either reached the end of the spline or reached end point within tolerance.
+         * In case tolerance not reached, allow 1 second period of adjustment
+         */
+
+        ElapsedTime et = new ElapsedTime();
+
+        double targetHeading = endHeadingRadians;
+        if (reverse) {
+            targetHeading = AngleUnit.normalizeRadians(targetHeading + Math.PI);
+        }
+
+        while (opModeIsActive() && et.seconds() < 1){
+            bot.updateOdometry();
+            VectorF vecToEnd = endVec.subtracted(new VectorF((float)bot.getPose().x, (float)bot.getPose().y));
+            headingError = AngleUnit.normalizeRadians(targetHeading - bot.getPose().h);
+            if (vecToEnd.magnitude() < tolerance && Math.abs(headingError) < Math.toRadians(3)) break;
+            vel = vecToEnd.multiplied(8);
+            velRobot = fieldToBot(vel, bot.getPose().h);
+            va = 4.0 * headingError;
+            bot.setDriveSpeed(velRobot.get(0), velRobot.get(1), va);
+        }
+
+        bot.setDriveSpeed(0,0,0);
+    }
+
+
+
+
+    protected VectorF fieldToBot(VectorF vField, double heading) {
+        float sinTheta = (float) Math.sin(heading);
+        float cosTheta = (float) Math.cos(heading);
+        return new VectorF(vField.get(0) * sinTheta - vField.get(1) * cosTheta, vField.get(0) * cosTheta + vField.get(1) * sinTheta);
+    }
+
+    protected VectorF botToField(VectorF vBot, double heading){
+        float sinTheta = (float) Math.sin(heading);
+        float cosTheta = (float) Math.cos(heading);
+        return new VectorF(vBot.get(0) * sinTheta + vBot.get(1) * cosTheta, -vBot.get(0) * cosTheta + vBot.get(1) * sinTheta);
+    }
+
+
+
+
 
 
 }
--- TeamCode/src/org/firstinspires/ftc/teamcode/mybot/MyAutoTest.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/mybot/MyAutoTest.java
@@ -18,7 +18,7 @@ public class MyAutoTest extends MyAuto {
         setBot(bot);
 
         // Tell the MyBot object where it is starting on the field (coordinates and orientation)
-        bot.setPose(0, 0, 90);
+        bot.setPose(0, 0, 0);
 
         // Wait for the start button to be pressed
         waitForStart();
@@ -26,13 +26,33 @@ public class MyAutoTest extends MyAuto {
         /* Drive straight from the starting position to the specified coordinates, while maintaining the
            specified orientation.
          */
-        driveTo(new MotionProfile(10, 30, 10), 48, 0, 90, 1);
+//        driveTo(new MotionProfile(10, 30, 10), 48, 0, 90, 1);
 
         // Turn to the specified heading
-        turnTo(180, 90, 6, 1);
+//        turnTo(180, 90, 6, 1);
 
         // Drive straight to new specified position
-        driveTo(new MotionProfile(10, 30, 10), 0, 48, 180, 1);
+//        driveTo(new MotionProfile(10, 30, 10), 0, 48, 180, 1);
+
+//        turnTo(0, 90, 6, 1);
+
+        MotionProfile slow = new MotionProfile(5, 15, 5);
+
+        MotionProfile fast = new MotionProfile(15, 50, 20);
+
+        MotionProfile normal = new MotionProfile(10, 30, 10);
+
+//        driveTo(slow, 0, -24, 0, 1);
+
+//        driveTo(fast, 0, 24, 0, 1);
+
+//        turnTo(-90, 90, 6, 1);
+
+//        driveTo(slow, 48, 24, -90, 1);
+
+        splineTo(normal, 0, 48, 90, 1, false);
+
+        
 
     }
 
